9ac32b7ca5188eba087d7752584e351ba1cd5e0b
AlexeyKiriluk <gromsl96@gmail.com>
2022-02-21 16:23:30 -0500

    m

--- server/yolov5/main.py
+++ server/yolov5/main.py
@@ -11,7 +11,7 @@ model = torch.hub.load(f"{absolutePath}\\yolov5", "custom", path=f"{absolutePath
 results = model(img)
 
 results.show()
-results.save(f"{absolutePath}/data/processed")
+results.save(f"{absolutePath}\\data\\{pano_id}\\processed")
 
 print(results.pandas().xyxy[0].to_json(orient="records"))
 
